c7528d4b2d1be8514c8123754dc02f27fd1de930
ukaleem <ukaleem540@gmail.com>
2022-02-22 13:58:27 +0500

    new changes in rroutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {environment} from '../environments/environment';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
